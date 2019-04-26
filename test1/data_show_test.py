ac6daf149afa800938e704690528bdd9d9fb2a5e
曹海涛 <cht@xingzheyi.com>
2019-04-26 10:14:18 +0800

    test1测试通过

---
+++ test1/data_show_test.py
@@ -0,0 +1,92 @@
+from train import *
+import os
+from torchvision import transforms as T
+import json
+import torch
+from PIL import Image
+import cv2
+from PIL import Image,ImageDraw,ImageFont
+import numpy as np
+import random
+
+classics = {
+    0:u"red",
+    1:u"green",
+    2:u"blue",
+    3:u"yellow",
+    4:u"Cyan"
+}
+
+def read_one_pic(img_path):
+    normalize = T.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+
+    transforms = T.Compose([
+        T.ToTensor(),
+        normalize
+    ])
+
+    data = Image.open(img_path)
+    data = transforms(data)
+    data = data.reshape(1,data.size()[0],data.size()[1],data.size(2))
+    return data
+
+def draw_by_one_data(i,pic_path,data1,data2):
+    index_x = int(i % 13)
+    index_y = int(i / 13)
+    sub_x = data1[0] * 32
+    sub_y = data1[1] * 32
+    center_x = sub_x + 32 * index_x
+    center_y = sub_y + 32 * index_y
+    width = data1[2] * 416
+    height = data1[3] * 416
+
+    classic = torch.argmax(data2)
+    rect = [center_x-width/2,center_y-height/2,center_x+width/2,center_y+height/2]
+
+    img = Image.open(pic_path)
+    draw = ImageDraw.Draw(img)
+    xs = [rect[0], rect[2]]
+    ys = [rect[1], rect[3]]
+    draw.line((xs[0], ys[0], xs[1], ys[0]), fill=(0, 0, 0),width=2)
+    draw.line((xs[1], ys[0], xs[1], ys[1]), fill=(0, 0, 0),width=2)
+    draw.line((xs[1], ys[1], xs[0], ys[1]), fill=(0, 0, 0),width=2)
+    draw.line((xs[0], ys[1], xs[0], ys[0]), fill=(0, 0, 0),width=2)
+    index = int(classic)-1
+    t = classics[index]
+    draw.text((10,20),t,fill=(0,0,0))
+
+    img = np.array(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    cv2.imshow('test', img)
+    cv2.waitKey(0)
+
+
+def cac_one_pic_and_save():
+    root = r"D:\code\PythonCode\tobject_detect\test1\imgs\imgs"
+    pic_paths = [os.path.join(root,img) for img in os.listdir(root)]
+    pic_paths = pic_paths[int(0.7*len(pic_paths)):]
+    random.shuffle(pic_paths)
+
+    if not os.path.exists(pkl_name):
+        print("can not find pkl(%s)"%pkl_name)
+        return
+    cnn = torch.load(pkl_name).cpu().eval()
+
+    for pic_path in pic_paths:
+        img_data = read_one_pic(pic_path)
+        result = cnn(img_data)
+        result = result.reshape(result.size()[1],result.size()[2])
+        results = torch.split(result,[5,6],1)
+        for i in range(results[0].size()[0]):
+            data1 = results[0][i]
+            data2 = results[1][i]
+            if (data1[4]<0.6):
+                continue
+            draw_by_one_data(i,pic_path,data1,data2)
+        pass
+
+
+if __name__=='__main__':
+    cac_one_pic_and_save()
